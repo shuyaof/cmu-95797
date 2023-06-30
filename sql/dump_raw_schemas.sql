ae6406fce1266dac82c9a628667f0af75ccf1ffe
“Vera <shuyaof@andrew.cmu.edu>
2023-06-30 11:22:08 -0400

    sql commands

---
+++ sql/dump_raw_schemas.sql
@@ -0,0 +1,16 @@
+.echo on
+
+-- print a list of table names
+SHOW TABLES;
+
+--print a list of schemas
+DESCRIBE bike_data;
+DESCRIBE central_park_weather;
+DESCRIBE daily_citi_bike_trip_counts_and_weather;
+DESCRIBE fhv_bases;
+DESCRIBE fhv_tripdata;
+DESCRIBE fhvhv_tripdata;
+DESCRIBE taxi_yellow;
+DESCRIBE taxi_green;
+
+
---
+++ sql/ingest.sql
@@ -0,0 +1,11 @@
+-- create tables in duckdb
+
+CREATE TABLE bike_data AS SELECT * FROM read_csv_auto('data/bike/202*.csv.gz', union_by_name=True, sample_size=-1);
+CREATE TABLE central_park_weather AS SELECT * FROM 'data/central_park_weather.csv';
+CREATE TABLE daily_citi_bike_trip_counts_and_weather AS SELECT * FROM 'data/daily_citi_bike_trip_counts_and_weather.csv';
+CREATE TABLE fhv_bases AS SELECT * FROM 'data/fhv_bases.csv';
+CREATE TABLE fhv_tripdata AS SELECT * FROM 'data/taxi/fhv_tripdata_*.parquet';
+CREATE TABLE fhvhv_tripdata AS SELECT * FROM 'data/taxi/fhvhv_tripdata_*.parquet';
+CREATE TABLE taxi_yellow AS SELECT * FROM 'data/taxi/yellow_*.parquet';
+CREATE TABLE taxi_green AS SELECT * FROM 'data/taxi/green_*.parquet';
+
