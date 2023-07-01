62b05c3ec7c1c5442391482ec2d13f3b4316384f
“Vera <shuyaof@andrew.cmu.edu>
2023-06-30 22:52:07 -0400

    alter tables

--- sql/dump_raw_schemas.sql
+++ sql/dump_raw_schemas.sql
@@ -3,6 +3,15 @@
 -- print a list of table names
 SHOW TABLES;
 
+ALTER TABLE bike_data ADD filename VARCHAR;
+ALTER TABLE central_park_weather ADD filename VARCHAR;
+ALTER TABLE daily_citi_bike_trip_counts_and_weather ADD filename VARCHAR;
+ALTER TABLE fhv_bases ADD filename VARCHAR;
+ALTER TABLE fhv_tripdata ADD filename VARCHAR;
+ALTER TABLE fhvhv_tripdata ADD filename VARCHAR;
+ALTER TABLE taxi_yellow ADD filename VARCHAR;
+ALTER TABLE taxi_green ADD filename VARCHAR;
+
 --print a list of schemas
 DESCRIBE bike_data;
 DESCRIBE central_park_weather;
