9eff902c81920c3b2842e231c77208e378f99feb
“Vera <shuyaof@andrew.cmu.edu>
2023-07-05 21:23:57 -0400

    solution keys

--- sql/dump_raw_schemas.sql
+++ sql/dump_raw_schemas.sql
@@ -1,25 +1,11 @@
 .echo on
 
--- print a list of table names
 SHOW TABLES;
-
-ALTER TABLE bike_data ADD filename VARCHAR;
-ALTER TABLE central_park_weather ADD filename VARCHAR;
-ALTER TABLE daily_citi_bike_trip_counts_and_weather ADD filename VARCHAR;
-ALTER TABLE fhv_bases ADD filename VARCHAR;
-ALTER TABLE fhv_tripdata ADD filename VARCHAR;
-ALTER TABLE fhvhv_tripdata ADD filename VARCHAR;
-ALTER TABLE taxi_yellow ADD filename VARCHAR;
-ALTER TABLE taxi_green ADD filename VARCHAR;
-
---print a list of schemas
-DESCRIBE bike_data;
-DESCRIBE central_park_weather;
-DESCRIBE daily_citi_bike_trip_counts_and_weather;
-DESCRIBE fhv_bases;
-DESCRIBE fhv_tripdata;
-DESCRIBE fhvhv_tripdata;
-DESCRIBE taxi_yellow;
-DESCRIBE taxi_green;
-
-
+DESCRIBE "bike_data";
+DESCRIBE "central_park_weather";
+DESCRIBE "daily_citi_bike_trip_counts_and_weather";
+DESCRIBE "fhv_bases";
+DESCRIBE "fhv_tripdata";
+DESCRIBE "fhvhv_tripdata";
+DESCRIBE "green_tripdata";
+DESCRIBE "yellow_tripdata";
