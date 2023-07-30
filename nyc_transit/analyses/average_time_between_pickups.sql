f5caafc13d38a5e2891a72cb9570e4674817655f
“Vera <shuyaof@andrew.cmu.edu>
2023-07-30 09:10:31 -0400

    final touches

--- nyc_transit/analyses/average_time_between_pickups.sql
+++ nyc_transit/analyses/average_time_between_pickups.sql
@@ -1,19 +1,28 @@
---Find the average time between taxi pick ups per zone
---● Use lead or lag to find the next trip per zone for each record
---● then find the time difference between the pick up time for the current record and the next
---● then use this result to calculate the average time between pick ups per zone.
+-- Find the average time between taxi pick ups per zone
+-- Use lead or lag to find the next trip per zone for each record
+-- then find the time difference between the pick up time for the current record and the next
+-- then use this result to calculate the average time between pick ups per zone.
 
-with pickup_zone as (select 
-       zone,
-       datediff('second',pickup_datetime,lead(pickup_datetime) 
-       over (partition by zone order by pickup_datetime)) as difference
-from {{ ref('mart__fact_all_taxi_trips') }} t
-join {{ ref('mart__dim_locations') }} l on l.locationid = t.pulocationid
--- where zone = 'Westerleigh'
-)
+-- depends_on: {{ ref('mart__fact_all_taxi_trips') }}
+{% set zones = dbt_utils.get_column_values(
+    table=ref('mart__dim_locations'),
+    column='Zone') %}
 
-select pickup_zone.zone as zone,
-       avg(pickup_zone.difference) as average_time_between_pickups
-from pickup_zone
-group by zone
+{% for zone in zones %}
+    {% set zone = zone.replace("'", "''") %} --change single quote to double quote to avoid the error
+
+    SELECT 
+        zone,
+        AVG(difference) AS average_time_between_pickups
+    FROM
+    (
+        SELECT 
+            zone,datediff('second', t.pickup_datetime, lead(t.pickup_datetime) OVER (ORDER BY t.pickup_datetime)) AS difference
+        FROM {{ ref('mart__fact_all_taxi_trips') }} t
+        JOIN {{ ref('mart__dim_locations') }} l 
+        ON l.locationid = t.pulocationid
+        WHERE l.zone = '{{ zone }}'
+    ) AS time_diffs
+    group by 1;
+{% endfor %}
  
--- nyc_transit/analyses/days_before_precip_more_bike_trips.sql
+++ nyc_transit/analyses/days_before_precip_more_bike_trips.sql
@@ -1,17 +1,32 @@
 -- determine if days immediately preceding precipitation or snow had more bike trips on average than the days with precipitation or snow.
 
-with bike_weather as 
-      (
-       SELECT date, sum(trips) as trips,precipitation, snowfall, 
-       CASE WHEN (precipitation > 0 OR snowfall > 0) THEN 1 ELSE 0 END AS is_bad_weather
-FROM {{ ref('stg__daily_citi_bike_trip_counts_and_weather') }}
-group by all),
+WITH bike_trips AS (
+    SELECT 
+    date_trunc('day', started_at_ts)::date as date,
+	count(*) as trips
+	from {{ ref('mart__fact_all_bike_trips') }}
+group by all
+),
 
-lagged_data AS (
+weather AS (
+    SELECT w.date, w.prcp, w.snow, 
+        CASE WHEN (w.prcp > 0 OR w.snow > 0) THEN 1 ELSE 0 END AS is_bad_weather
+    FROM {{ ref('stg__central_park_weather') }} w
+),
+
+weather_and_bike_trips AS (
+    SELECT w.date, w.is_bad_weather, COALESCE(b.trips, 0) as trips
+    FROM weather w
+    inner JOIN bike_trips b ON w.date = b.date
+),
+
+lead_data AS (
     SELECT date, trips, is_bad_weather, 
-        lead(is_bad_weather) OVER (ORDER BY date) AS next_day_weather
-    FROM bike_weather)
+        lead(is_bad_weather) OVER (ORDER BY date) AS lead_day_weather
+    FROM weather_and_bike_trips
+)
 
-SELECT AVG(CASE WHEN next_day_weather = 1 THEN trips ELSE NULL END) AS avg_trips_on_previous_bad_weather_days,
+
+SELECT AVG(CASE WHEN lead_day_weather = 1 THEN trips ELSE NULL END) AS avg_trips_on_previous_bad_weather_days,
        AVG(CASE WHEN is_bad_weather = 1 THEN trips ELSE NULL END) AS avg_trips_on_bad_weather_days
-FROM lagged_data
+FROM lead_data
